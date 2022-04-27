ab7e53037591297c26e486a717d3063a4497b1fe
ozkavosh <augustoslva@hotmail.com>
2022-04-27 16:12:32 -0300

    Ediciones Desafio 2 de React

--- react/desafio2/src/components/NavBar.jsx
+++ react/desafio2/src/components/NavBar.jsx
@@ -10,10 +10,10 @@ const NavBar = () => {
     <nav className="navContainer">
         <h1 className="navBrand"> Renac<span className="textoBlanco">ER</span> {title} </h1>
         <ul className="navButtons">
-            <li className="navButton"> <a href="#" onClick={() => setTitle("")}>Home</a> </li>
-            <li className="navButton"> <a href="#" onClick={() => setTitle("| Nosotros")}>Nosotros</a> </li>
-            <li className="navButton"> <a href="#" onClick={() => setTitle("| Contacto")}>Contacto</a> </li>
-            <li className="navButton"> <a href="#" onClick={() => setTitle("| Cuenta")}><FontAwesomeIcon icon={faUserCircle}/></a> </li>
+            <li className="navButton"> <button onClick={() => setTitle("")}>Home</button> </li>
+            <li className="navButton"> <button onClick={() => setTitle("| Nosotros")}>Nosotros</button> </li>
+            <li className="navButton"> <button onClick={() => setTitle("| Contacto")}>Contacto</button> </li>
+            <li className="navButton"> <button className="botonCuenta" onClick={() => setTitle("| Cuenta")}><FontAwesomeIcon icon={faUserCircle}/></button> </li>
         </ul>
     </nav>
   );
